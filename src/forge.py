8b49afc3f7b4304ea4ce4f58481fbef7c191f2ac
JacobCompany <jacob.gross@health-union.com>
2025-06-03 14:07:32 -0400

    Add comments and check save location is correct

--- src/forge.py
+++ src/forge.py
@@ -17,13 +17,15 @@ def forge(model_version: str = "Meta-Llama-3-8B-Instruct.Q4_0.gguf"):
     # Setup log
     log = []
 
-    # Query model
+    # Initialize model
     try:
         model = GPT4All(model_version)
     except ValueError:
         print("Model ('{0}') was not found, please provide a different model.".format(model_version))
     else:
+        # Open up chat session
         with model.chat_session():
+            # Check that user doesn't want to exit
             while query.lower() not in exit_conditions:
                 # Save user query
                 log.append("{0}\nQuery: {1}".format("-" * 15, query))
@@ -37,8 +39,11 @@ def forge(model_version: str = "Meta-Llama-3-8B-Instruct.Q4_0.gguf"):
                 # Get user's query
                 query = input("Query: ")
 
+        # Save log
         if input("Save log (Y/N)? ").lower() == "y":
             save_loc = input("Save location: ")
+            while not save_loc.endswith(".txt"):
+                save_loc = input("Save location must be .txt, not {0}\nSave location: ".format(save_loc))
             with open(save_loc, "w") as outfile:
                 outfile.write("\n".join(log))
                 outfile.close()
