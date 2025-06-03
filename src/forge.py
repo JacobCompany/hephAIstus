d3e7b4548c913da828752d362e9cdb8618635626
JacobCompany <jacob.gross@health-union.com>
2025-06-03 12:18:13 -0400

    Make forge function

--- src/forge.py
+++ src/forge.py
@@ -1,10 +1,22 @@
 from gpt4all import GPT4All
 
-# Getting user's query
-query = input("Query: ")
-print("Generating response...")
 
-# Query model
-model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
-with model.chat_session():
-    print(model.generate(query, max_tokens=1024))
+def forge(model_version: str = "Meta-Llama-3-8B-Instruct.Q4_0.gguf"):
+    """
+    Simple AI query tool
+
+    :param model_version:
+    str: The model to use in the query
+    """
+    # Getting user's query
+    query = input("Query: ")
+    print("Generating response...")
+
+    # Query model
+    model = GPT4All(model_version)
+    with model.chat_session():
+        print(model.generate(query, max_tokens=1024))
+
+
+if __name__ == "__main__":
+    forge()
