226e21127692b327fea5524eb95efacb423f9e4b
JacobCompany <jacob.gross@health-union.com>
2025-06-04 12:58:05 -0400

    Allow user to load previous convo

--- src/hephaestus/god_of_forges.py
+++ src/hephaestus/god_of_forges.py
@@ -34,6 +34,9 @@ waiting_messages = [
     "Waiting for Hermes to return",
 ]
 
+# Define split condition
+new_query_text = "-" * 15
+
 
 class Hephaestus:
     def __init__(self):
@@ -62,7 +65,7 @@ class Hephaestus:
                 return
             elif log["role"] == "user":
                 logs_formatted.append(
-                    "{0}\nQuery: {1}\n{0}".format("-" * 15, log["content"])
+                    "{0}\nQuery: {1}\n{0}".format(new_query_text, log["content"])
                 )
             else:
                 logs_formatted.append(log["content"])
@@ -91,19 +94,39 @@ class Hephaestus:
             outfile.close()
             print("Saved logs to {0}".format(save_loc))
 
-    def _load_logs(self, file_name: str):
+    def load_logs(self, file_name: str):
         """
         Loads logs to be used by ollama
         """
         # Reset logs
         self._reset_logs()
 
+        # Initialize logs
+        logs_unformatted = []
+
         # Open the log file
         with open(file_name, "r") as input_file:
-            input_file.read()
-            self.logs = []
+            # Read the entire log file
+            logs_file = input_file.read()
+            # Run through each query/response
+            for content in logs_file.split(new_query_text):
+                # Strip content
+                content = content.strip()
 
-        # Set that logs were loaded
+                # Ensure there is actually a query/response
+                if len(content) > 0:
+                    # Get role
+                    if content.startswith("Query: "):
+                        role = "user"
+                        content = content.replace("Query: ", "")
+                    else:
+                        role = "assistant"
+
+                    # Add query/response
+                    logs_unformatted.append({"role": role, "content": content})
+
+        # Update logs
+        self.logs = logs_unformatted
         self.logs_loaded = True
 
     def forge(self, model_version: str = "Meta-Llama-3-8B-Instruct.Q4_0.gguf"):
@@ -134,13 +157,15 @@ class Hephaestus:
                 # Check that user doesn't want to exit
                 while query.lower() not in exit_conditions:
                     # Save user query
-                    self.logs.append("{0}\nQuery: {1}\n{0}".format("-" * 15, query))
+                    self.logs.append(
+                        "{0}\nQuery: {1}\n{0}".format(new_query_text, query)
+                    )
 
                     # Get response from model
                     print("{0}...".format(random.choice(waiting_messages)))
                     response = model.generate(query, max_tokens=1024) + "\n"
                     self.logs.append(response)
-                    print("{0}\n{1}".format("-"*15, response))
+                    print("{0}\n{1}".format(new_query_text, response))
 
                     # Get user's query
                     query = input("Query: ")
@@ -183,7 +208,7 @@ class Hephaestus:
                 self.logs.append(
                     {"role": "assistant", "content": response.message.content}
                 )
-                print("{0}\n{1}".format("-"*15, response.message.content))
+                print("{0}\n{1}".format(new_query_text, response.message.content))
 
                 # Get user's query
                 query = input("\nQuery: ")
